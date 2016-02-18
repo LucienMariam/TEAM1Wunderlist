32c3948ce30f42325812b8732d5a9f982713f8ce
LucienMariam <Alcotrass@protonmail.ch>
2016-02-19 02:44:59 +0300

    Add concrete entities

---
+++ DAL/Concrete/Entities/TaskUserDAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DAL.Interfaces.Entities;
+
+namespace DAL.Concrete.Entities
+{
+    class TaskUserDAL: IDALEntity
+    {
+        public Guid UserId { get; set; }
+        public Guid TaskId { get; set; }
+    }
+}
