158f405486f046458ea85095246495e84b50fe9c
LucienMariam <Alcotrass@protonmail.ch>
2016-02-20 14:16:26 +0300

    Change DAL classes access
    Forgot to set public access modificator.

--- DAL/Interfaces/Mappers/IMapperDAL.cs
+++ DAL/Interfaces/Mappers/IMapperDAL.cs
@@ -3,7 +3,7 @@ using DAL.Interfaces.Entities;
 
 namespace DAL.Interfaces.Mappers
 {
-    interface IMapperDAL< TORMEntity, TDALEntity> where TORMEntity : IORMEntity
+    public interface IMapperDAL< TORMEntity, TDALEntity> where TORMEntity : IORMEntity
                                                   where TDALEntity : IDALEntity
     {
         TORMEntity ToORM(TDALEntity entity);
