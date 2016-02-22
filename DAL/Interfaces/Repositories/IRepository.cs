6aabe39018755b377eb5f3161de6abea9d5d2519
LucienMariam <Alcotrass@protonmail.ch>
2016-02-22 03:25:08 +0300

    Add repository interfaces

---
+++ DAL/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using DAL.Interfaces.Entities;
+
+namespace DAL.Interfaces.Repositories
+{
+    public interface IRepository<TEntity> where TEntity: IDALEntity
+    {
+        IEnumerable<TEntity> GetAll();
+
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
