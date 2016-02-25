adfaf1d6ea509238d3b6faed0b9af9fc1c67884c
YauheniTretsyak <tretsyak.yauheni@gmail.com>
2016-02-25 21:21:21 +0300

    Add content, views, User's controller, some models for registration and logIn

---
+++ TaskManager/Models/TaskUserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class TaskUserModel
+    {
+        [Display(Name = "User")]
+        public string UserLogin { get; set; }
+
+        [Required]        
+        public Guid UserId { get; set; }
+
+        [Display(Name = "Task")]
+        public string TaskTitle { get; set; }
+
+        [Required]
+        public Guid TaskId { get; set; }
+
+        [Required]
+        [Display(Name = "Progress")]        
+        public int Progress { get; set; }
+    }
+}
