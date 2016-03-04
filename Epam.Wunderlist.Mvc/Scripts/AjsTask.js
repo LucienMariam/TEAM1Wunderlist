bc1988085a6182f26bfb2b0b4f984629c4b5c1c2
YauheniTretsyak <tretsyak.yauheni@gmail.com>
2016-03-04 17:34:36 +0300

    AddListOfTasks

---
+++ Epam.Wunderlist.Mvc/Scripts/AjsTask.js
@@ -0,0 +1,40 @@
+﻿angular.module('AngularJS').controller('TasksCtrl', function ($scope, $http) {
+    $scope.tasks = [{}];
+    
+    // Get customer list 
+    $scope.get = function (email) {
+        $http.get('/api/Task/' + email)
+       .success(function (response) {
+           $scope.tasks = response;
+       });}
+    
+
+
+
+    $scope.addAlert = function (email) {
+        //
+        var temp = document.getElementById('newListTaskList').value;
+        var value = {
+            Title: temp,
+        };
+
+        $.post("api/Task",
+           value,
+            function (value) {
+                // Refresh list   
+                $http.get('/api/Task/' + email)
+                 .success(function (response) {
+                     $scope.tasks.push({ Title: temp });
+                 });
+            },
+            "json"
+           );
+            
+    };
+
+    $scope.closeAlert = function (index, title) {
+        $http.delete('api/Task/' + title).success(function () {
+            $scope.tasks.splice(index, 1);
+        });}
+         
+})
