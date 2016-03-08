817d81f711ec9f346d1e825caa45411a0b638f69
LucienMariam <Alcotrass@protonmail.ch>
2016-03-08 04:09:08 +0300

    Inject task service

--- Epam.Wunderlist.Services/Concrete/Services/TaskService.cs
+++ Epam.Wunderlist.Services/Concrete/Services/TaskService.cs
@@ -1,18 +1,47 @@
-﻿using DAL.Concrete.Entities;
+﻿using System;
+using DAL.Concrete.Entities;
 using BLL.Interfaces.Services;
 using DAL.Interfaces;
 using DAL.Interfaces.Repositories;
 using BLL.Concrete.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BLL.Services
 {
-    public class TaskService: KeyService<TaskDal, TaskEntity, ITaskRepository, TaskMapper>, ITaskService
+    public class TaskService : KeyService<TaskDal, TaskEntity, ITaskRepository, TaskMapper>, ITaskService
     {
-        private readonly ITaskRepository _repository;
+        private readonly ITaskRepository _taskRepository;
 
         public TaskService(ITaskRepository repository, IUnitOfWork contextChanger) : base(repository, contextChanger)
         {
-            _repository = repository;
+            _taskRepository = Repository;
+        }
+
+        public IEnumerable<TaskEntity> GetTaskList(int folderId)
+        {
+            return _taskRepository.GetTaskList(folderId).Select(dalEntity => EntityMapper.ToBll(dalEntity));
+        }
+
+        public void Rename(int id, string newTitle)
+        {
+            TaskEntity task = EntityMapper.ToBll(_taskRepository.GetById(id));
+            task.Title = newTitle;
+            Edit(task);
+        }
+
+        public void AddNote(int id, string note)
+        {
+            TaskEntity task = EntityMapper.ToBll(_taskRepository.GetById(id));
+            task.Description = note;
+            Edit(task);
+        }
+
+        public void SetDate(int id, DateTime? date)
+        {
+            TaskEntity task = EntityMapper.ToBll(_taskRepository.GetById(id));
+            task.DueTime = date;
+            Edit(task);
         }
     }
 }
--- Epam.Wunderlist.Services/Interfaces/Services/ITaskService.cs
+++ Epam.Wunderlist.Services/Interfaces/Services/ITaskService.cs
@@ -1,10 +1,14 @@
 ﻿using BLL.Concrete.Entities;
 using System.Collections.Generic;
+using System;
 
 namespace BLL.Interfaces.Services
 {
     public interface ITaskService: IKeyService<TaskEntity>
     {
         IEnumerable<TaskEntity> GetTaskList(int folderId);
+        void Rename(int id, string newTitle);
+        void AddNote(int id, string note);
+        void SetDate(int id, DateTime? date);
     }
 }
