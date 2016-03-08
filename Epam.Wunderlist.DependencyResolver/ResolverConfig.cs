ffcad60cb01d0ed454079c45aeb320dbfbaf616a
LucienMariam <Alcotrass@protonmail.ch>
2016-03-08 04:42:46 +0300

    Reconfigure bindings

--- Epam.Wunderlist.DependencyResolver/ResolverConfig.cs
+++ Epam.Wunderlist.DependencyResolver/ResolverConfig.cs
@@ -9,6 +9,8 @@ using DAL.Concrete.Mappers;
 using BLL.Interfaces.Services;
 using BLL.Concrete.Entities;
 using BLL;
+using BLL.Concrete.Services;
+using BLL.Services;
 using DAL.Interfaces;
 using DAL.Concrete;
 
@@ -22,22 +24,24 @@ namespace CustomNinjectDependencyResolver
             kernel.Bind<DbContext>().To<EntityModel>().InRequestScope();
 
             #region Repository binding
-            kernel.Bind<IKeyRepository<TaskDal>>().To<KeyRepository<Task, TaskDal, TaskMapperDal>>();
-            kernel.Bind<IKeyRepository<UserDal>>().To<KeyRepository<User, UserDal, UserMapperDal>>();
-            kernel.Bind<ITaskUserRepository>().To<TaskUserRepository>();
-            kernel.Bind<IUserRepository>().To<UserRepository>();
-            kernel.Bind<ITaskRepository>().To<TaskRepository>();
-            kernel.Bind<IFolderRepository>().To<FolderRepository>();
+            kernel.Bind<IKeyRepository<TaskDal>>().To<KeyRepository<Task, TaskDal, TaskMapperDal>>().InSingletonScope();
+            kernel.Bind<IKeyRepository<UserDal>>().To<KeyRepository<User, UserDal, UserMapperDal>>().InSingletonScope();
+            kernel.Bind<ITaskUserRepository>().To<TaskUserRepository>().InSingletonScope();
+            kernel.Bind<IUserRepository>().To<UserRepository>().InSingletonScope();
+            kernel.Bind<ITaskRepository>().To<TaskRepository>().InSingletonScope();
+            kernel.Bind<IFolderRepository>().To<FolderRepository>().InSingletonScope();
             #endregion
 
             #region Service binding
-            kernel.Bind<IKeyService<TaskEntity>>().To<KeyService<TaskDal, TaskEntity, IKeyRepository<TaskDal>, TaskMapper>>();
-            kernel.Bind<IKeyService<UserEntity>>().To<KeyService<UserDal, UserEntity, IKeyRepository<UserDal>, UserMapper>>();
-            kernel.Bind<ITaskUserService>().To<TaskUserService>();
-            kernel.Bind<IUserService>().To<UserService>();
+            kernel.Bind<IKeyService<TaskEntity>>().To<KeyService<TaskDal, TaskEntity, IKeyRepository<TaskDal>, TaskMapper>>().InSingletonScope();
+            kernel.Bind<IKeyService<UserEntity>>().To<KeyService<UserDal, UserEntity, IKeyRepository<UserDal>, UserMapper>>().InSingletonScope();
+            kernel.Bind<ITaskUserService>().To<TaskUserService>().InSingletonScope();
+            kernel.Bind<IUserService>().To<UserService>().InSingletonScope();
+            kernel.Bind<ITaskService>().To<TaskService>().InSingletonScope();
+            kernel.Bind<IFolderService>().To<FolderService>().InSingletonScope();
             #endregion
 
-            kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InSingletonScope();
         }
 
         public static void Reconfiguration(this IKernel kernel)
