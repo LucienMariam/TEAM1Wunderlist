f8719734bf4d7ce59eae9966c8efe8f644d7d0eb
YauheniTretsyak <tretsyak.yauheni@gmail.com>
2016-03-15 19:32:16 +0300

    addApportunityForCRUDInAngular(withTaskObjects)

--- Epam.Wunderlist.Mvc/Controllers/FolderController.cs
+++ Epam.Wunderlist.Mvc/Controllers/FolderController.cs
@@ -23,8 +23,15 @@ namespace TaskManager.Controllers
         [ActionName("DefaultAction")]
         public IEnumerable<FolderModel> Get(int id)
         {
-            var folderList = folders.GetAll().Where(x=>x.UserId==id).Select(x=>new FolderModel() { Id = x.Id, ParentFolderId = x.ParentFolderId,UserId = x.UserId, Title = x.Title});
+            var folderList = folders.GetAll().Where(x => x.UserId == id).Select(x => new FolderModel() { Id = x.Id, ParentFolderId = x.ParentFolderId, UserId = x.UserId, Title = x.Title });
             return folderList;
+        }
+
+        [ActionName("GetFolderById")]
+        public FolderModel GetFolderById(int id)
+        {
+            var folder = folders.GetById(id);
+            return new FolderModel() { Id = folder.Id, ParentFolderId = folder.ParentFolderId, UserId = folder.UserId, Title = folder.Title };
         }
         [ActionName("GetByParentId")]
         public IEnumerable<FolderModel> GetByParentId(int id)
@@ -32,20 +39,35 @@ namespace TaskManager.Controllers
             var folderList = folders.GetAll().Where(x => x.ParentFolderId == id).Select(x => new FolderModel() { Id = x.Id, ParentFolderId = x.ParentFolderId, UserId = x.UserId, Title = x.Title });
             return folderList;
         }
-        // POST: api/Task
+
         [ActionName("DefaultAction")]
         public void Post(FolderModel value)
         {
-            folders.Add(new FolderEntity() { ParentFolderId= value.ParentFolderId, Title= value.Title, UserId= value.UserId});
+            folders.Add(new FolderEntity() { ParentFolderId = value.ParentFolderId, Title = value.Title, UserId = value.UserId });
         }
 
-        // PUT: api/Task/5
+        [ActionName("PostFolder")]
+        public void PostFolder(FolderModel value)
+        {
+
+
+            var folderForUpdate = new FolderEntity()
+            {
+                Id = value.Id,
+                UserId = int.Parse(User.Identity.Name),
+                ParentFolderId = null,
+                Title = value.Title
+            };
+            folders.Edit(folderForUpdate);
+
+        }
+
         [ActionName("DefaultAction")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE: api/Task/5
+
         [ActionName("DefaultAction")]
         public void Delete(int id)
         {
