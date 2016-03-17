b539ec984dfc9af0a4a5d5274eeff77defba7331
LucienMariam <Alcotrass@protonmail.ch>
2016-03-18 02:54:00 +0300

    Add photo service interface

--- Epam.Wunderlist.PhotoService/IPhotoService.cs
+++ Epam.Wunderlist.PhotoService/IPhotoService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ServiceAbstraction;
+using BLL.Entities;
 
 namespace Epam.Wunderlist.PhotoService
 {
-    public interface IPhotoService
+    public interface IPhotoService: IService<UserEntity>
     {
     }
 }
